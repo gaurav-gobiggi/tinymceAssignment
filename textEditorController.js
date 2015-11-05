a906960b4b91eb2f763e9a596e0a5e5f1e0839fe
Gaurav Pokharna <vikas.s.satpute@gmail.com>
2015-11-05 18:57:10 +0530

    First commit

--- textEditorController.js
+++ textEditorController.js
@@ -1,92 +1,96 @@
-  var app = angular.module('textEditorApp', []);
+var app = angular.module('textEditorApp', ['ui.tinymce']);
+app.controller('textEditorController', function($scope, $http, $sce) {
+    $scope.editText = {
+        'text1': 'Click here to edit abc!',
+        'text2': 'Click here to edit!',
+        'text3': 'Click here to edit!',
+        'text4': 'Click here to edit!'
+    };
+    var ctrl = this;
 
-app.controller('textEditorController', function($scope, $http) {
+    $scope.abc = {
+        inline: true
+    };
 
-     	 $scope.editText = {
-        	'text1' : 'Click here to edit!',
-        	'text2' : 'Click here to edit!',
-        	'text3' : 'Click here to edit!',
-        	'text4' : 'Click here to edit!',
+    $scope.tinymceOptions = {
+        //trusted: true,
+        inline: true,
+        menubar: false,
+        toolbar: "bold italic underline styleselect",
+        statusbar: false,
+        plugins: 'advlist autolink link image lists charmap print preview',
+        skin: 'lightgray',
+        theme: 'modern'
+    };
+
+    $scope.updateHtml = function() {
+        console.log('test', $scope.editText);
+        ctrl.tinymceHtml = $sce.trustAsHtml($scope.editText.text1);
+    };
+    $scope.content = function() {
+        console.log($scope.editText);
+        // $scope.editText.text1 = tinyMCE.get('myeditablediv').getBody().textContent;
+        // $scope.editText.text2 = tinyMCE.get('myeditablediv1').getBody().textContent;
+        // $scope.editText.text3 = tinyMCE.get('myeditablediv2').getBody().textContent;
+        // $scope.editText.text4 = tinyMCE.get('myeditablediv3').getBody().textConten,t;
+        // console.log($scope.editText.text1);
+        // console.log($scope.editText.text2);
+        // console.log($scope.editText.text3);
+        // console.log($scope.editText.text4);
+    };
+    //     tinymce.init({
+    //          selector: "#myeditablediv",
+    //          inline: true,
+    //          menubar: false,
+    //          toolbar: "bold italic underline styleselect",
+    // statusbar: false
+    //      });
+    //  tinymce.init({
+    //          selector: "#myeditablediv1",
+    //          inline: true,
+    //          menubar: false,
+    //          toolbar: "bold italic underline styleselect",
+    // statusbar: false
+    //      });
+    //  tinymce.init({
+    //          selector: "#myeditablediv2",
+    //          inline: true,
+    //          menubar: false,
+    //          toolbar: "bold italic underline styleselect",
+    // statusbar: false
+    //      });
+    //          tinymce.init({
+    //          selector: "#myeditablediv3",
+    //          inline: true,
+    //          menubar: false,
+    //          toolbar: "bold italic underline styleselect",
+    // statusbar: false
+    //      });
+    /*$scope.content= function(index) {
+            console.log("index",index);
+            $scope.editText[index] = tinyMCE.activeEditor.getBody().textContent;
+            console.log($scope.editText[index]);
+            console.log($scope.editText);
+        }*/
+    /*$scope.content1= function() {
+            $scope.editText.text2= tinyMCE.activeEditor.getBody().textContent;
+            console.log($scope.editText.text2);
+            console.log($scope.editText);
         };
 
-       tinymce.init({
-            selector: "#myeditablediv",
-            inline: true,
-            menubar: false,
-            toolbar: "bold italic underline styleselect",
-			statusbar: false
-
-        });
-
-	   tinymce.init({
-            selector: "#myeditablediv1",
-            inline: true,
-            menubar: false,
-            toolbar: "bold italic underline styleselect",
-			statusbar: false
-
-        });
-
-	   tinymce.init({
-            selector: "#myeditablediv2",
-            inline: true,
-            menubar: false,
-            toolbar: "bold italic underline styleselect",
-			statusbar: false
-
-        });
-
-	   		tinymce.init({
-            selector: "#myeditablediv3",
-            inline: true,
-            menubar: false,
-            toolbar: "bold italic underline styleselect",
-			statusbar: false
-
-        });
-       
-        /*$scope.content= function(index) {
-        	console.log("index",index);
-			$scope.editText[index] = tinyMCE.activeEditor.getBody().textContent;
-			console.log($scope.editText[index]);
-			console.log($scope.editText);
-		}*/
-
-		  $scope.content= function() {
-		  	$scope.editText.text1 = tinyMCE.get('myeditablediv').getBody().textContent;
-		  	$scope.editText.text2 = tinyMCE.get('myeditablediv1').getBody().textContent;
-		  	$scope.editText.text3 = tinyMCE.get('myeditablediv2').getBody().textContent;
-        	$scope.editText.text4 = tinyMCE.get('myeditablediv3').getBody().textContent;
-        	console.log($scope.editText.text1);
-        	console.log($scope.editText.text2);
-        	console.log($scope.editText.text3);
-        	console.log($scope.editText.text4);
-        	console.log($scope.editText);
-		};
-
-
-
-        /*$scope.content1= function() {
-        	$scope.editText.text2= tinyMCE.activeEditor.getBody().textContent;
-			console.log($scope.editText.text2);
-			console.log($scope.editText);
-		};
-
-		
+        
 
         $scope.content2= function() {
-        	$scope.editText.text3= tinyMCE.activeEditor.getBody().textContent;
-			console.log($scope.editText.text3);
-			console.log($scope.editText);
-		};
+            $scope.editText.text3= tinyMCE.activeEditor.getBody().textContent;
+            console.log($scope.editText.text3);
+            console.log($scope.editText);
+        };
 
 
 
         $scope.content3= function() {
-        	$scope.editText.text4= tinyMCE.activeEditor.getBody().textContent;
-			console.log($scope.editText.text4);
-			console.log($scope.editText);
-		};*/
-
-
-     });
+            $scope.editText.text4= tinyMCE.activeEditor.getBody().textContent;
+            console.log($scope.editText.text4);
+            console.log($scope.editText);
+        };*/
+});
